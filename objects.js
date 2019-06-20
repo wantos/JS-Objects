8f89c25689d0da40629734513980fdd0cdf86812
Wantono Emmanuel <51445540+wantos@users.noreply.github.com>
2019-06-20 09:56:48 +0300

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,27 @@
+var contact = new details(firstName, secondName, phoneNumber, email);
+
+contact.firstName = 'emma';
+contact.secondName = 'wantos';
+contact.phoneNumber = '+256708885671';
+contact.email = 'wantos.outbox@gmail.com';
+
+var myAddress = {
+	street: william street;
+	 city: kampala;
+	country: uganda;
+};
+
+function phonedetail( first, second, number, mail)
+ {
+  this.firstName = 'emma';
+  this.secondName = 'wantono';
+  this.phonenumber = '+256708885671';
+  this.email = 'mail';
+     
+
+phonedetail.prototype.name = function() {
+  return this.firstName + " " + this.secondName;
+    };
+ };
+console.log(phonedetail);
+
